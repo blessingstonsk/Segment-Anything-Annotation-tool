7527a6d0b54f937ad705c3e3b8954295d82aa682
rith-19 <rithishkumarravi2@gmail.com>
2024-04-26 11:48:41 +0530

    File added on 26/04/2024

--- utils/download_model.py
+++ utils/download_model.py
@@ -1,29 +1,20 @@
 import os
-import urllib.request
-
 
 def download_model(model_type):
     """
     model_type: str, A string representing the model type. It can be 'vit_h', 'vit_l', or 'vit_b'.
     """
 
-    # A dictionary containing model types as keys and their respective URLs as values
-    model_urls = {
-        "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
-        "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
-        "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
-    }
-
-    # Check if the model file already exists and model_type is in model_urls
+    # Check if the model file already exists
     filename = f"{model_type}.pth"
-    if not os.path.exists(filename) and model_type in model_urls:
-        url = model_urls[model_type]
-        print(f"Downloading {model_type} model from {url}...")
-        urllib.request.urlretrieve(url, filename)
-        print(f"{model_type} model has been successfully downloaded and saved as '{filename}'.")
-    elif os.path.exists(filename):
-        print(f"{model_type} model already exists as '{filename}'. Skipping download.")
+    if os.path.exists(filename):
+        print(f"{model_type} model already exists as '{filename}'. Using existing model file.")
     else:
-        raise ValueError("Invalid model type. It should be 'vit_h', 'vit_l', or 'vit_b'.")
+        raise FileNotFoundError(f"{model_type} model file '{filename}' not found in the root directory.")
 
     return filename
+
+# Example usage:
+model_type = "vit_h"
+model_file = download_model(model_type)
+print("Model file path:", model_file)
