54b729520ee514c9a7fdcbf90216c79d82da9481
rith-19 <rithishkumarravi2@gmail.com>
2024-04-26 12:02:43 +0530

    File added on 26/04/2024 and modified

--- annotator.py
+++ annotator.py
@@ -50,16 +50,14 @@ class MainWindow(QtWidgets.QMainWindow):
     
     FIT_WINDOW, FIT_WIDTH, MANUAL_ZOOM = 0, 1, 2
     
-    def __init__(self, parent=None, global_w=1000, global_h=1800, model_type='vit_b', keep_input_size=True, max_size=1080):
+    def __init__(self, parent=None, global_w=1000, global_h=1600, model_type='vit_h', keep_input_size=True, max_size=720):
         super(MainWindow, self).__init__(parent)
         self.resize(global_w, global_h)
         self.model_type = model_type
         self.keep_input_size = keep_input_size
-        self.max_size = float(max_size)
+        self.max_size = max_size
 
         self.setWindowTitle('NEVAR Systems')
-
-        # Set the window icon
         self.setWindowIcon(QIcon("Nevarlogo.png")) 
 
         self.canvas = Canvas(self,
